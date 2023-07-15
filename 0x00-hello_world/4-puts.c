005382757858d7fead53c8a9735c444fb8188574
root <zamombonambi39@gmail.com  git config --global user.name zamo9>
2023-07-15 12:22:39 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * Return : Always0 (success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
 
