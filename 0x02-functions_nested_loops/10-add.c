2e6434e305c974828028095e43400731adb1bb7c
root <zamombonambi39@gmail.com  git config --global user.name zamo9>
2023-07-18 15:36:39 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n1: The first number to be added
+ * @n2: The second number to be added
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
+
+
+	
